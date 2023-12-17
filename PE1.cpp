dba7654ceae8b4b15469997fc1c4b420e21e2844
chris samuel <112623896+chrisracha@users.noreply.github.com>
2023-12-17 20:38:54 +0800

    Update PE1.cpp

--- PE1.cpp
+++ PE1.cpp
@@ -2,18 +2,18 @@
 Chris Samuel Salcedo
 Ann Junah Suyman
 2022-05055
-2022-
+2022-09089
 
 CMSC 122
 Programming Exercise 01: List Implementation (Array)
 
-This program implements a list ADT using an array.
+This program implements a list ADT using an array implementation.
 */
-
 #include <iostream>
 
-using namespace std;
 #define MAXSIZE 10 // define max size of list
+
+using namespace std;
 
 // define struct list
 struct list {
@@ -23,8 +23,8 @@ struct list {
 
 // declare functions
 int MENU();
-void INSERT(int item, int p, struct list* L);
-void DELETE(int p, struct list* L);
+bool INSERT(int item, int p, struct list* L);
+bool DELETE(int p, struct list* L);
 void DISPLAY(struct list* L);
 void DELETELIST(struct list* L);
 
@@ -42,16 +42,48 @@ while(1) {
     switch (choice)
     {
     case (1):
-        cout << "Enter item to add: ";
-        cin >> item;
-        cout << "Enter position to add: ";
-        cin >> p;
-        INSERT(item , p, &L); // pass by reference
+        cout << "Where to add the item?" << endl;
+        cout << "[1] Start of the list\n[2] End of the list\n[3] Specific position\n[0] Cancel" << endl << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
+        switch (choice)
+        {
+        case (1):
+            p = 0;
+            cout << "Enter item to add: ";
+            cin >> item;
+            if(!INSERT(item , p, &L)) { // pass by reference
+            cout << "Insertion failed\n";
+            }
+            break;
+        case (2):
+            p = L.end + 1;
+            cout << "Enter item to add: ";
+            cin >> item;
+            if(!INSERT(item , p, &L)) { // pass by reference
+            cout << "Insertion failed\n";
+            }
+            break;
+        case (3):
+            cout << "Enter position to add: ";
+            cin >> p;
+            cout << "Enter item to add: ";
+            cin >> item;
+            if(!INSERT(item , p, &L)) // pass by reference
+                cout << "Insertion failed\n";
+            break;
+                case (0):
+            break;
+        default:
+            cout << "Invalid Choice";
+            break;
+        }
         break;
     case (2):
         cout << "Enter position to delete: ";
         cin >> p;
-        DELETE(p, &L);
+        if(!DELETE(p, &L))
+            cout << "Deletion failed\n";
         break;
     case (3):
         DISPLAY(&L);
@@ -60,7 +92,7 @@ while(1) {
         DELETELIST(&L);
         break;
     case (0):
-        cout << "Progam Exited Successfully";
+        cout << "Program Exited Successfully";
         return 0;
         break;
     default:
@@ -76,12 +108,12 @@ return 0;
 // show menu
 int MENU() {
     int choice;
-    cout << "List MENU\n\n";
-    cout << "[1] Add Item\n";
-    cout << "[2] Delete Item\n";
-    cout << "[3] Display List\n";
-    cout << "[4] Delete List\n";
-    cout << "[0] Exit Program\n\n";
+    cout << "\nList MENU" << endl << endl;
+    cout << "[1] Add Item" << endl;
+    cout << "[2] Delete Item" << endl;
+    cout << "[3] Display List" << endl;
+    cout << "[4] Delete List" << endl;
+    cout << "[0] Exit Program" << endl << endl;
 
     cout << "Enter choice: ";
     cin >> choice;
@@ -89,51 +121,51 @@ int MENU() {
 }
 
 // insert item
-void INSERT(int item, int p, struct list* L) {
-    int i;
-    if (L->end >= MAXSIZE) { // check if list is full
-        cout << "List is full\n";
-        return;
+bool INSERT(int item, int p, struct list* L) {
+    if (L->end + 1 >= MAXSIZE) { // check if list is full
+        cout << "List is full" << endl;
+        return false;
     } else if (p > L->end + 1 || p < 0) { // check if position is valid
-        cout << "Invalid position\n";
-        return;
+        return false;
     } else {
-        for (i = L->end; i >= p; i--) { // shift items to the right
+        for (int i = L->end; i >= p; i--) { // shift items to the right
             L->items[i+1] = L->items[i];
         }
         // insert item at position p and increment end
         L->items[p] = item;
         L->end++;
+        return true;
     }
 }
 
 // delete item
-void DELETE(int p, struct list* L) {
-    int i;
+bool DELETE(int p, struct list* L) {
+    int item;
+    item = L->items[p];
     if (L->end < 0) { // check if list is empty
-        cout << "List is empty\n";
-        return;
+        return false;
     } else if (p > L->end || p < 0) { // check if position is valid
-        cout << "Invalid position\n";
-        return;
+        return false;
     } else {
-        for (i = p; i < L->end; i++) {
+        for (int i = p; i < L->end; i++) {
             L->items[i] = L->items[i+1]; // shift items to the left
         }
         L->end--; // decrement end
+        cout << "The element " << item <<  ", which is at position " << p << " has been deleted." << endl;
+        return true;
     }
 }
 
 // display list
 void DISPLAY(struct list* L) {
-    int i;
-
     if (L->end < 0) {
-        cout << "List is empty\n";
+        cout << "List is empty" << endl;
         return;
     } else {
-        for (i = 0; i <= L->end; i++) {
-            cout << L->items[i] << "\n\n"; // loop through items and display
+        cout << endl;
+        for (int i = 0; i <= L->end; i++) {
+            cout << "[" << i << "] " << flush;
+            cout << L->items[i] << endl; // loop through items and display
         }
     }
 }
@@ -141,4 +173,5 @@ void DISPLAY(struct list* L) {
 // delete list
 void DELETELIST(struct list* L) {
     L->end = -1; // set end to -1 to empty list
+    cout << "The list has been emptied!" << endl;
 }
